e15837378e95c6efdfa3a41ccb485b936677f0be
Shivam Sharma <kshivam654@gmail.com>
2019-10-18 00:51:10 +0530

    click on register fixed

--- app/src/main/java/app/sharma/com/Main2Activity.java
+++ app/src/main/java/app/sharma/com/Main2Activity.java
@@ -3,12 +3,27 @@ package app.sharma.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main2);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
