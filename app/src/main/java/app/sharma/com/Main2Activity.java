0d6a55fda9f7bd6372dbcbe4d5cac4d608369c89
Shivam Sharma <kshivam654@gmail.com>
2019-10-18 22:43:34 +0530

    views initialized

--- app/src/main/java/app/sharma/com/Main2Activity.java
+++ app/src/main/java/app/sharma/com/Main2Activity.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
     TextView back;
+
+    EditText name, email, password, cpassword, year, branch, group;
+
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +24,31 @@ public class Main2Activity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main2);
-        back = findViewById(R.id.back);
+
+        back = (TextView) findViewById(R.id.back);
+        name = (EditText) findViewById(R.id.name);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        cpassword = (EditText) findViewById(R.id.confirmpassword);
+        year = (EditText) findViewById(R.id.year);
+        branch = (EditText) findViewById(R.id.branch);
+        group = (EditText) findViewById(R.id.group);
+
+        signup = (Button) findViewById(R.id.signup);
+
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!password.getText().toString().equals(cpassword.getText().toString())){
+                    
+                }
             }
         });
     }
