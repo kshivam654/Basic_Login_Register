e3035c424e558ccfad6f9c40e7f2b1074c3b5d87
Shivam Sharma <kshivam654@gmail.com>
2019-10-18 22:30:44 +0530

    permissions done + firebase added

--- app/src/main/java/app/sharma/com/Main2Activity.java
+++ app/src/main/java/app/sharma/com/Main2Activity.java
@@ -22,7 +22,7 @@ public class Main2Activity extends AppCompatActivity {
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();//
+                finish();
             }
         });
     }
