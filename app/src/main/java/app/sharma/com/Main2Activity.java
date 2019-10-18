17a69b506d747b0c92731d08264ab916c8b62825
Shivam Sharma <kshivam654@gmail.com>
2019-10-18 22:59:52 +0530

    signup code done

--- app/src/main/java/app/sharma/com/Main2Activity.java
+++ app/src/main/java/app/sharma/com/Main2Activity.java
@@ -1,16 +1,25 @@
 package app.sharma.com;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -20,12 +29,16 @@ public class Main2Activity extends AppCompatActivity {
 
     Button signup;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main2);
+
+        mAuth = FirebaseAuth.getInstance();
 
         back = (TextView) findViewById(R.id.back);
         name = (EditText) findViewById(R.id.name);
@@ -50,8 +63,44 @@ public class Main2Activity extends AppCompatActivity {
             public void onClick(View v) {
                 if(!password.getText().toString().equals(cpassword.getText().toString())){
                     Snackbar.make(v,"Passwords need to be same...", Snackbar.LENGTH_LONG).show();
+
+                    startRegis(email,password);
+
                 }
             }
         });
+    }
+
+    private void startRegis(EditText e, EditText p) {
+
+        String email, password;
+        email = e.getText().toString();
+        password = p.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
     }
 }
