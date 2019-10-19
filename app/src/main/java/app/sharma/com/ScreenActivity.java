36dc35530f5246c64976794057762429bd94f126
Shivam Sharma <kshivam654@gmail.com>
2019-10-19 20:10:00 +0530

    floating action button added

--- app/src/main/java/app/sharma/com/ScreenActivity.java
+++ app/src/main/java/app/sharma/com/ScreenActivity.java
@@ -2,13 +2,30 @@ package app.sharma.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ScreenActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingactionbutton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScreenActivity.this, RequestActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
